b409d828093aca091faba7be73c1a987a74d38fb
Dominique Demetz <ddemetz123@icloud.com>
2018-07-13 18:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //Code your solution here
+function getFirstSelector (selector){
+  return document.querySelector (selector);
+}
+
+function nestedTarget(){
+  return document.querySelector ('#nested .target');
+}
+
+function increaseRankBy(n){
+  
+const rankedList =  document.querySelectorAll ('.ranked-list');
+
+  for (let i = 0; i < 4; i++){
+    rankedList[i].parseInt = (i + 1);
+  }
+}
+
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
